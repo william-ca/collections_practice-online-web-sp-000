131603c4313e6553d1b714e5a951e9401374d8a5
Will Andre <wandre94@gmail.com>
2021-01-02 23:04:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,15 @@ def sort_array_desc(ary)
     end
   end
 end
+
+def sort_array_char_count(ary)
+  ary.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end 
+  end
+end
